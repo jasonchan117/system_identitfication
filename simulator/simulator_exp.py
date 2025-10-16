3e876fad79018a94c091945bfa2d751ab1bb5b3c
Jasonchan_Lab <18933818702@163.com>
2025-10-15 21:02:28 -0400

    fix bug

--- simulator/simulator_exp.py
+++ simulator/simulator_exp.py
@@ -374,6 +374,8 @@ class ExpSimulator:
             stress = (-self.dt[None] * self.p_vol[p] * 4 * self.inv_dx[None]**2) * stress
             # calculate the momentum of two consecutive frames at grid node
             stress_next = (-self.dt[None] * self.p_vol[p] * 4 * self.inv_dx[None]**2) * stress_next
+            print("stress:" stress) 
+            print("stress_next:" stress_next)
             for i in ti.static(range(3)):
                 for j in ti.static(range(3)):
                     for k in ti.static(range(3)):
@@ -390,7 +392,7 @@ class ExpSimulator:
 
                         self.grid_f[base + offset] += weight * stress @ dpos
                         self.grid_f_next[base_next + offset] += weight_next * stress_next @ dpos_next
-                        print(">>", weight_next, stress_next, dpos_next)
+                        
                         # if weight_next != weight_next:
                         #     self.nan_flag[None] = 1
                         '''
